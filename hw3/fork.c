4ca92452fc8fc87024bcd5f5ca59f6a9405697eb
Sam Hopkins <samhopkins@Sams-MacBook-Air-3.local>
2019-09-15 12:42:45 -0600

    Finished HW

--- hw3/fork.c
+++ hw3/fork.c
@@ -6,12 +6,17 @@
 int main(int argc, char *argv[]) {
 	int pid;
     FILE *fp;
+    FILE *fp2;
+    int p[2];
+    FILE *stream;
     
     fp = fopen("fork-output.txt", "w");
 	printf("Starting program; process has pid %d\n", getpid());
 
 	fprintf(fp, "BEFORE FORK\n");
 	fflush(fp);
+    
+    pipe(p);
 
 	if ((pid = fork()) < 0) {
 		fprintf(stderr, "Could not fork()");
@@ -21,18 +26,36 @@ int main(int argc, char *argv[]) {
 	/* BEGIN SECTION A */
     fprintf(fp, "Section A\n");
     fflush(fp);
-//	printf("Section A;  pid %d\n", getpid());
-	//sleep(30);
+    printf("Section A;  pid %d\n", getpid());
 
 	/* END SECTION A */
 	if (pid == 0) {
 		/* BEGIN SECTION B */
 
-//		printf("Section B\n");
+        close(p[0]);
+        stream = fdopen(p[1], "w");
+        fputs("hello from Section B\n", stream);
+        
+		printf("Section B\n");
         fprintf(fp, "Section B\n");
         fflush(fp);
-		//sleep(30);
+        
 		printf("Section B done sleeping\n");
+        
+        char *newenviron[] = { NULL };
+        
+        printf("Program \"%s\" has pid %d. Sleeping.\n", argv[0], getpid());
+        
+        if (argc <= 1) {
+            printf("No program to exec.  Exiting...\n");
+            exit(0);
+        }
+        
+        printf("Running exec of \"%s\"\n", argv[1]);
+        dup2(fileno(fp), STDOUT_FILENO);
+        fclose(fp);
+        execve(argv[1], &argv[1], newenviron);
+        printf("End of program \"%s\".\n", argv[0]);
         
 		exit(0);
 
@@ -40,12 +63,17 @@ int main(int argc, char *argv[]) {
 	} else {
 		/* BEGIN SECTION C */
         
-//		printf("Section C\n");
+		printf("Section C\n");
         fprintf(fp, "Section C\n");
-	fflush(fp);
-		//sleep(30);
+        fflush(fp);
+        
+        close(p[1]);
+        stream = fdopen(p[0], "r");
+        char readString [100];
+        fgets(readString, 100, stream);
+        printf("%s", readString);
+
         wait(0);
-		//sleep(30);
 		printf("Section C done sleeping\n");
 
 		exit(0);
@@ -57,7 +85,6 @@ int main(int argc, char *argv[]) {
 	printf("Section D\n");
     fprintf(fp, "Section D\n");
     fflush(fp);
-	//sleep(30);
 
 	/* END SECTION D */
 }

84463f7b69462d3e3e2e243c6491a21a02093260
Clotonervo <sam.hopkins@blastsoccer.net>
2019-09-21 10:56:15 -0600

    Finished quit output

--- Shell Lab/tsh.c
+++ Shell Lab/tsh.c
@@ -247,7 +247,7 @@ int parseline(const char *cmdline, char **argv)
 int builtin_cmd(char **argv) 
 {
     if (strcmp(argv[0], "quit") == 0){
-        printf("quit\n");
+        printf("%s quit\n", prompt);
         exit(0);
     }
     else if (strstr(argv[0], "fg") != NULL){

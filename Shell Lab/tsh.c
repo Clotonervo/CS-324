1550fa6211e93282c706a754143d64a77177bb8c
Clotonervo <sam.hopkins@blastsoccer.net>
2019-09-21 11:36:47 -0600

    Commented out quit output because I think it is stupid to have output for quit command

--- Shell Lab/tsh.c
+++ Shell Lab/tsh.c
@@ -166,11 +166,11 @@ int main(int argc, char **argv)
 void eval(char *cmdline) 
 {
 	char *argv[MAXARGS];
-	int bg;
+	int background;
 	pid_t pid;
 
 
-	bg = parseline(cmdline, argv);
+	background = parseline(cmdline, argv);
     if (argv[0] == NULL){
         return;
     }
@@ -247,7 +247,7 @@ int parseline(const char *cmdline, char **argv)
 int builtin_cmd(char **argv) 
 {
     if (strcmp(argv[0], "quit") == 0){
-        printf("%squit\n", prompt);
+        //printf("%squit\n", prompt);
         exit(0);
     }
     else if (strstr(argv[0], "fg") != NULL){

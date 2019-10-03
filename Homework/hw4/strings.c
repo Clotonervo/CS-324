b3da0915c73e625809ce504cf793147dbb2af20c
Samuel R. Hopkins <srhopkins@mastercontrol.com>
2019-10-02 18:56:55 -0600

    Partway done with h4 part 2

--- Homework/hw4/strings.c
+++ Homework/hw4/strings.c
@@ -12,20 +12,21 @@
 //#1: Check if str equals the string "Hello World!"
 //You may assume that str is null-terminated
 int isHelloWorld(char* str) {
-    return (str == "Hello World!");
+    return (!strcmp("Hello World!", str));
 }
 
 //#2: Replace str with the string "Hello World!"
 //You may assume that str is long enough to hold 'Hello World!'
 void fillHelloWorld(char* str) {
-    str = "Hello World!";
+    strcpy(str, "Hello World!");
 }
 
 //#3: Return a new string that is a copy of str
 //You may assume that str is null-terminated
 char* copyString(char* str) {
-    char newStr[strlen(str)+1];
+    char* newStr = (char*)malloc((strlen(str)+1));
     strcpy(newStr, str);
+
     return newStr;
 }
 
@@ -34,9 +35,9 @@ char* copyString(char* str) {
 //NOTE: You will need to replicate the solution you did for Part 3 to fix an identical bug in this part.
 //However, that is not the only issue with this code.
 //HINT: You may need to add another argument to the function to make this work.
-char* copyArray(char* array) {
-    char newArray[sizeof(array)];
-    memcpy(newArray, array, sizeof(array));
+char* copyArray(char* array, int arraySize) {
+    char* newArray = (char*)malloc(arraySize);
+    memcpy(newArray, array, arraySize);
     return newArray;
 }
 
@@ -86,13 +87,13 @@ int main(int argc, char* argv[]) {
             printf("Invalid test number: %s\n", argv[1]);
             return 1;
         case 1:
-	        printf("RUNNING TEST 1 - isHelloWorld(char* str)\nCheck if str equals the string 'Hello World!'\n\n");
+            printf("RUNNING TEST 1 - isHelloWorld(char* str)\nCheck if str equals the string 'Hello World!'\n\n");
             h1 = isHelloWorld(hello);
             printf("Input: str='%s'\nValue returned from function: %d (%s)\n", hello, h1, h1 ? "True" : "False");
-	        printf("Expected: True\n\n");
+            printf("Expected: True\n\n");
             h2 = isHelloWorld(hello2);
             printf("Input: '%s'\nValue returned from function: %d (%s)\n", hello2, h2, h2 ? "True" : "False");
-	        printf("Expected: False\n");
+            printf("Expected: False\n");
             printf(h1 && !h2 ? SMSG : FMSG);
             return 0;
         case 2:
@@ -100,7 +101,7 @@ int main(int argc, char* argv[]) {
             printf("Input: str='%s'\n", str);
             fillHelloWorld(str);
             printf("Value of str after function call: '%s'\n", str);
-	        printf("Expected: 'Hello World!'\n");
+            printf("Expected: 'Hello World!'\n");
             printf(strcmp(str, "Hello World!") == 0 ? SMSG : FMSG);
             return 0;
         case 3:
@@ -108,55 +109,55 @@ int main(int argc, char* argv[]) {
             printf("Input: str='%s'\n", str);
             cpy = copyString(str);
             printf("String returned from function: '%s'\n", cpy);
-	        printf("Expected: '%s'\n", str);
-	        if (str == cpy) {
-		        printf("\nTEST FAILED: Input and output pointers are identical - this is not a copy\n");
-		        return 0;
-	        }
+            printf("Expected: '%s'\n", str);
+            if (str == cpy) {
+                printf("\nTEST FAILED: Input and output pointers are identical - this is not a copy\n");
+                return 0;
+            }
             printf(strcmp(cpy, str) == 0 ? SMSG : FMSG);
             return 0;
         case 4:
-	        printf("RUNNING TEST 4 - copyArray(char* array)\nReturn a new character array that is a copy of the passed-in array\n\n");
+            printf("RUNNING TEST 4 - copyArray(char* array)\nReturn a new character array that is a copy of the passed-in array\n\n");
             printf("Input: bytes=");
-	        printBytes(bytes, sizeof(bytes));
-            cpy = copyArray(bytes); //You may change this line by adding another argument to copyArray
-	        printf("Array returned from function (first %ld bytes): ", sizeof(bytes));
-	        printBytes(cpy, sizeof(bytes));
-	        printf("Expected: ");
             printBytes(bytes, sizeof(bytes));
-	        if (bytes == cpy) {
+            cpy = copyArray(bytes, sizeof(bytes)); //You may change this line by adding another argument to copyArray
+            printf("Array returned from function (first %ld bytes): ", sizeof(bytes));
+            printBytes(cpy, sizeof(bytes));
+            printf("Expected: ");
+            printBytes(bytes, sizeof(bytes));
+            if (bytes == cpy) {
                 printf("\nTEST FAILED: Input and output pointers are identical - this is not a copy\n");
                 return 0;
             }
             printf(memcmp(bytes, cpy, sizeof(bytes)) == 0 ? SMSG : FMSG);
             return 0;
         case 5:
-	        printf("RUNNING TEST 5 - getSubstring(char* str, unsigned int begin, unsigned int length)\nGet the substring in str of the given begin index and length\n\n");
-	        printf("Input: str='%s', begin=3, length=7\n", hello);
-	        word = getSubstring(hello, 3, 7);
-	        printf("String returned from function: '%s'\n", word);
-	        printf("Expected: '%s'\n", expectedSubstr);
-	        printf(strcmp(word, expectedSubstr) == 0 ? SMSG : FMSG);
-	        return 0;
-	    case 6:
-	        printf("RUNNING TEST 6 - getFirstWord(char* str)\nReturn the first word of str (space delimited) without modifying str\n\n");
-	        printf("Input: str='%s'\n", str);
-	        word = getFirstWord(str);
-	        printf("String returned from function: '%s'\n", word);
+            printf("RUNNING TEST 5 - getSubstring(char* str, unsigned int begin, unsigned int length)\nGet the substring in str of the given begin index and length\n\n");
+            printf("Input: str='%s', begin=3, length=7\n", hello);
+            word = getSubstring(hello, 3, 7);
+            printf("String returned from function: '%s'\n", word);
+            printf("Expected: '%s'\n", expectedSubstr);
+            printf(strcmp(word, expectedSubstr) == 0 ? SMSG : FMSG);
+            return 0;
+        case 6:
+            printf("RUNNING TEST 6 - getFirstWord(char* str)\nReturn the first word of str (space delimited) without modifying str\n\n");
+            printf("Input: str='%s'\n", str);
+            word = getFirstWord(str);
+            printf("String returned from function: '%s'\n", word);
             printf("Expected: '%s'\n", expectedFirst);
             printf("Value of str after function call: '%s'\n", str);
-	        printf(strcmp(word, expectedFirst) != 0 ? FMSG :
+            printf(strcmp(word, expectedFirst) != 0 ? FMSG :
                     strcmp(str, expected) != 0 ? "TEST FAILED: The input string was modified by the function\n" : SMSG);
-	        return 0;
-	    default:
-	        printf("Invalid test number: %s\n", argv[1]);
+            return 0;
+        default:
+            printf("Invalid test number: %s\n", argv[1]);
             return 1;
     }
 }
 
 void printBytes(char* bytes, int size) {
     for (int i = 0; i < size; i++) {
-	printf("%d ", bytes[i]);
+    printf("%d ", bytes[i]);
     }
     printf("\n");
 }
